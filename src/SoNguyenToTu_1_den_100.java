d7e3a44d564ef3fd0599369efd997fbe5be2b154
1234567890 <phantiensy195@gmail.com>
2018-06-17 10:51:54 +0700

    Co_bao_nhieu_so_nguyen_to_tu_1_den_1000

--- src/SoNguyenToTu_1_den_100.java
+++ src/SoNguyenToTu_1_den_100.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class SoNguyenToTu_1_den_100 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("input :");
-        for (int number = 100; number > 2; number--){
-                int dem = 0;
-                for (int i = number - 1; i > 1; i--) {
-                    if (number % i == 0) {
-                        dem++;
-                    } else {
-                        dem = dem;
-                    }
-                }
-                if (dem == 0) {
-                    System.out.println(number + " la so nguyen to");
+        System.out.print("Bạn muốn in ra bao nhiêu số nguyên tố?: ");
+        int output = sc.nextInt();
+        for (int number = 3; number < 1000; number++) {
+            int count = 0;
+            int dem = 1;
+            for (int i = number - 1; i > 1; i--) {
+                if (number%i == 0) {
+                    dem++;
+                } else {
+                    dem = dem;
                 }
             }
+                if (dem == 1) {
+                    System.out.println(number + " la so nguyen to");
+                }
+        }
     }
 }
